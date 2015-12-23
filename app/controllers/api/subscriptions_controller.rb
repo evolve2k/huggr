9a63be1335249a1850b7ef2e74eb4f96b2b401e2
Richie Khoo <evolve2k@gmail.com>
2015-12-23 16:20:14 +1030

    Variable spec and guard clause

--- app/controllers/api/subscriptions_controller.rb
+++ app/controllers/api/subscriptions_controller.rb
@@ -1,10 +1,15 @@
 class Api::SubscriptionsController < ApplicationController
   def create
-    data = params[:data]
     render json: {
       data: {
-        id: "f019e02a-c6f2-4b27-9f2d-e9a342941f83"
+        id: data[:id]
       }
     }, status: 201
   end
+
+  private
+    def data
+      return { id: "f019e02a-c6f2-4b27-9f2d-e9a342941f83" } unless params[:data]
+      params[:data]
+    end
 end
